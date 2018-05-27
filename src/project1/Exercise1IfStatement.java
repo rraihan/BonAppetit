e3b45a206487f84f0f0f04287394e588071c3180
rraihan <dialraihan@gmail.com>
2018-05-27 13:05:32 -0400

    First commit on May 27 1:05 pm

---
+++ src/project1/Exercise1IfStatement.java
@@ -0,0 +1,22 @@
+package project1;
+
+public class Exercise1IfStatement {
+
+	public static void main(String[] args) {
+		
+		/*Car1 has  86000 mileage. 
+		 *Car2 has  101000 mileage.
+		 *Evaluate the output with if statement.
+		 */
+		
+		int bmw = 86000;
+		int jaguar = 101000;
+		
+		if(bmw<=jaguar) System.out.println("BMW has " + bmw +" "
+				+ "mileage" + " JAGUAR has " + jaguar + " mileage.");
+		
+		if (jaguar>=bmw) System.out.println("JAGUAR has more milage.");
+		
+	}
+
+}
